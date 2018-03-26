ab98780ab91b62506ccbbfb811d5b4c12d18423e
WangsungOh <wangsungoh@gmail.com>
2018-03-26 14:04:49 +0900

    메인화면 추가
    종료버튼 구현 완료

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,9 @@ import model.Meal;
 import model.Member;
 import model.OrderList;
 import util.Jdbc;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Main {
 	private JFrame frame;
@@ -53,8 +56,42 @@ public class Main {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setTitle("메인");
+		frame.setBounds(100, 100, 239, 195);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnRegUserButton = new JButton("사원등록");
+		btnRegUserButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnRegUserButton.setBounds(6, 5, 227, 37);
+		frame.getContentPane().add(btnRegUserButton);
+		
+		JButton btnUserButton = new JButton("사용자");
+		btnUserButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnUserButton.setBounds(6, 47, 227, 37);
+		frame.getContentPane().add(btnUserButton);
+		
+		JButton btnManagerButton = new JButton("관리자");
+		btnManagerButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnManagerButton.setBounds(6, 89, 227, 37);
+		frame.getContentPane().add(btnManagerButton);
+		
+		JButton btnExitButton = new JButton("종료");
+		btnExitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnExitButton.setBounds(6, 131, 227, 37);
+		frame.getContentPane().add(btnExitButton);
 	}
-
 }
